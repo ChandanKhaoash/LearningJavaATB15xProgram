6a0df927b882a584fc414ae1bc05fe781b785ef3
ChandanKhaoash <ckhaoash2406@gmail.com>
2025-12-10 08:23:19 +0530

    added a new sop statement

--- src/ex_01_Java_Basics/LAB001_HelloWorld.java
+++ src/ex_01_Java_Basics/LAB001_HelloWorld.java
@@ -3,6 +3,8 @@ package ex_01_Java_Basics;
 public class LAB001_HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello again");
+
     }
 
 
