c96faacc418899b9f92f0bb68234492e62126d95
ChandanKhaoash <ckhaoash2406@gmail.com>
2025-12-19 08:59:57 +0530

    opoerators push and commit

---
+++ src/ex_04_Keywords_Operators/Lab017_Operators.java
@@ -0,0 +1,17 @@
+package ex_04_Keywords_Operators;
+
+public class Lab017_Operators {
+    public static void main(String[] args) {
+        int a=10; //assigmnet operator
+        int b=20;
+        System.out.println(a+b);
+
+        float c= 28.987f;
+        System.out.println(b*c);
+        System.out.println(a+c);
+
+        int a1= -10;
+        int b1= -20;
+        System.out.println(a1+b1);
+    }
+}
