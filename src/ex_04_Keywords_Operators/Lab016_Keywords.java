4630d97518b26b7737f8dd5d1b65f95eb250d47d
ChandanKhaoash <ckhaoash2406@gmail.com>
2025-12-19 08:54:20 +0530

    keyword final

---
+++ src/ex_04_Keywords_Operators/Lab016_Keywords.java
@@ -0,0 +1,10 @@
+package ex_04_Keywords_Operators;
+
+public class Lab016_Keywords {
+    public static void main(String[] args) {
+        final float pi= 3.1414f;
+       // int pi= 3;
+        System.out.println("Pi value:" +pi);
+
+    }
+}
