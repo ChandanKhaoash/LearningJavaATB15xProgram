5903594ddadcbd962a9c8269856fc9035be4ef39
ChandanKhaoash <ckhaoash2406@gmail.com>
2025-12-19 08:39:42 +0530

    Variables and print statements

---
+++ src/ex_03_Java_Basics/Lab015_Variables.java
@@ -0,0 +1,18 @@
+package ex_03_Java_Basics;
+
+public class Lab015_Variables {
+    public static void main(String[] args) {
+        byte age_Chandan= 32;
+        // 32 here is called literal vzriable
+        short income_per_day= 8000;
+        int salary=2600000;
+        long mobile_no= 9937471245L;
+        char grade='A';
+        String name= "Chandan";
+
+        System.out.println("Name" +name);
+        System.out.printf("Hello my salary is %d and my name is %s", +salary,name);
+        System.out.print("Testing sysout print 1");
+        System.out.print("Testing sysout print 2");
+    }
+}
